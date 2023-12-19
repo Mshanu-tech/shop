93e501d741c6c627134eed34758fe1438653a0e5
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2023-12-19 20:45:38 +0530

    Login

--- src/router/UserRouter.jsx
+++ src/router/UserRouter.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import UserLogin from '../Pages/Users/UserLogin'
+import Home from '../Pages/Users/Home';
 function UserRouter() {
   console.log("user");
   return (
     <>
     <Routes>
-      <Route path={'/'} element={<UserLogin/>} />
+      <Route path={'/'} element={<Home/>} />
+      <Route path={'login'} element={<UserLogin/>} />
     </Routes>
     </>
   )
